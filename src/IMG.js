82c93692700c615e1fdd92388a387ca13a8688e2
MMostella <masonmostella@gmail.com>
2021-08-12 20:09:26 -0500

    Added styling to IMG

--- src/IMG.js
+++ src/IMG.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const StyledImg = styled.div `
+
+    img {
+        border-radius: 15px;
+        padding: 50px;
+        border: 2px solid black;
+    }
+`
+
 export default function IMG(props) {
     const { url } = props;
 
     return (
-        <>
+        <StyledImg >
             <img src={url}></img>
-        </>
+        </StyledImg >
     )
 }
---
+++ src/theme/index.js
@@ -0,0 +1,6 @@
+ export default {
+     primaryColor: '#942911',
+     secondaryColor: '#465362',
+     black: 'black',
+     white: 'white',
+ }
